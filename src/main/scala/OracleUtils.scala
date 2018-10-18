720e15acdd4d5ec1a53725269165d99276869240
lorenzo <lory.man95@gmail.com>
2018-10-18 14:36:21 +0200

    Fixed Oracle Insert

--- src/main/scala/OracleUtils.scala
+++ src/main/scala/OracleUtils.scala
@@ -1,10 +1,10 @@
-import java.util.Properties
+import java.util.{Calendar, Properties}
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
-private case class LexicalResOracle(var name:String, var value:Double, var idLemma:Long)
+private case class LexicalResOracle(var id:Long, var name:String, var value:Double, var idLemma:Long)
 private case class LemmaOracle(var id:Long, var name:String, var percentage:Double=0, var idFeeling:Long)
 private case class FeelingOracle(var id:Long, var name:String, var totalWords:Int=0)
 
@@ -41,15 +41,9 @@ object OracleUtils extends indexes {
 
   def FeelingToOracleObject(sqlContext:SparkSession, feelingList:List[Feeling], jdbcUrl:String, connectionProperties:Properties): Unit ={
     import sqlContext.implicits._
+    println("Started FeelingObjectToOracle: ",Calendar.getInstance())
 
-    val my_schema = StructType(Seq(
-      StructField("ID", LongType, nullable = false),
-      StructField("NAME", StringType, nullable = false),
-      StructField("VALUE", DoubleType),
-      StructField("ID_LEMMA", LongType, nullable = false)
-    ))
-
-    val tmpLexRes: DataFrame = sqlContext.createDataFrame(sqlContext.sparkContext.emptyRDD[Row], my_schema)
+    var tmpLexRes=List[LexicalResOracle]()
     var tmpLemma=List[LemmaOracle]()
     var tmpFeeling=List[FeelingOracle]()
 
@@ -58,18 +52,15 @@ object OracleUtils extends indexes {
       f.lemmas.foreach{l=>
         tmpLemma=new LemmaOracle(l.id,l.name,l.percentage,f.id)::tmpLemma
         //wtite lemma in table with feeling id
-        tmpLexRes.union(l.lexicalRes.toSeq.toDF()
-          .as[(String, Double)]  // <-- convert to typed Dataset as it makes map easier
-          .map { case (name,value) =>
-            (lexResIndex(),name, value, l.id) } // <-- add new column
-          .toDF("ID","NAME","VALUE","ID_LEMMA"))// <-- name the top-level fields
-
-        //l.lexicalRes to DF and write in Oracle
+        l.lexicalRes.foreach(lr=>tmpLexRes=new LexicalResOracle(lexResIndex(),lr._1,lr._2,l.id)::tmpLexRes)
       }
     }
+    println(tmpLexRes.length, tmpLexRes(0))
+    println("Finished FeelingObjectToOracle: ",Calendar.getInstance())
+
     OracleInsert(tmpFeeling.toDF("ID","NAME","TOTAL_WORDS"),"SYSTEM.MAADB_FEELING", jdbcUrl,connectionProperties)
     OracleInsert(tmpLemma.toDF("ID","NAME","PERCENTAGE","ID_FEELING"), "SYSTEM.MAADB_LEMMA", jdbcUrl,connectionProperties)
-    OracleInsert(tmpLexRes, "SYSTEM.MAADB_LEXICAL_RES", jdbcUrl,connectionProperties)
+    OracleInsert(tmpLexRes.toDF("ID","NAME","VALUE","ID_LEMMA"), "SYSTEM.MAADB_LEXICAL_RESOURCE", jdbcUrl,connectionProperties)
 
     //val Feeling_df:DataFrame = feelingList.toDF()
 
@@ -78,7 +69,7 @@ object OracleUtils extends indexes {
 
   def OracleInsert(dataFrame: DataFrame, tableName:String, jdbcUrl:String, connectionProperties:Properties): Unit ={
     //println(dataFrame)
-    dataFrame.repartition(100).write
+    dataFrame.repartition(10).write
       .mode("append")
       .jdbc(jdbcUrl, tableName, connectionProperties)
   }
